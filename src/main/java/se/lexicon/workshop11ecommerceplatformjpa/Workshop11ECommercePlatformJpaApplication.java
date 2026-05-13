ad530966eb374576bbf325b3233ac841a4ee233e
Kristy Heijenk <ksheijenk@hotmail.com>
2026-05-13 10:50:26 +0200

    first commit

---
+++ src/main/java/se/lexicon/workshop11ecommerceplatformjpa/Workshop11ECommercePlatformJpaApplication.java
@@ -0,0 +1,15 @@
+package se.lexicon.workshop11ecommerceplatformjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop11ECommercePlatformJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workshop11ECommercePlatformJpaApplication.class, args);
+    }
+
+    // start workshop
+
+}
---
+++ src/test/java/se/lexicon/workshop11ecommerceplatformjpa/Workshop11ECommercePlatformJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop11ecommerceplatformjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop11ECommercePlatformJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
