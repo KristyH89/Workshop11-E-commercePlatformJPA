65f33c555848f3f5280f0c6f96174157bbe2d8ce
Kristy Heijenk <ksheijenk@hotmail.com>
2026-06-09 16:00:53 +0200

    feat: add OrderResponseDto

---
+++ src/main/java/se/lexicon/dto/OrderResponseDto.java
@@ -0,0 +1,29 @@
+package se.lexicon.dto;
+
+import se.lexicon.entity.OrderStatus;
+
+import java.time.Instant;
+import java.util.List;
+
+// DTO returned after placing or fetching an order
+public record OrderResponseDto(
+
+        // Order ID
+        Long id,
+
+        // Timestamp when the order was created
+        Instant createdAt,
+
+        // ID of the customer who placed the order
+        Long customerId,
+
+        // Current order status (e.g., CREATED,PAID, CANCELLED)
+        OrderStatus status,
+
+        // Customer email or name (optional, but useful)
+        String customerEmail,
+
+        // List of items in the order
+        List<OrderItemResponseDto> items
+
+) {}
